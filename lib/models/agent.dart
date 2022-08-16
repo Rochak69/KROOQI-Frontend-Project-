a6c8f72b1e69ada9226cbddcc2e10f08c482298b
Rochak Shrestha <rochakshrestah666@gmail.com>
2022-08-16 11:39:41 +0545

    Provider used on agents

---
+++ lib/models/agent.dart
@@ -0,0 +1,8 @@
+class Agent {
+  String imageUrl, name, urduName;
+  Agent({
+    required this.imageUrl,
+    required this.name,
+    required this.urduName,
+  });
+}
---
+++ lib/models/rencent_enquiry.dart
@@ -0,0 +1,16 @@
+class RecentEnquiry {
+  String propertType,
+      propertyName,
+      enquiryDescription,
+      imageUrl,
+      companyName,
+      personName;
+  RecentEnquiry({
+    required this.propertType,
+    required this.propertyName,
+    required this.enquiryDescription,
+    required this.imageUrl,
+    required this.companyName,
+    required this.personName,
+  });
+}
